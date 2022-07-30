889198db9b0eb31bcd241b557a43e248fe66d436
GugaSantos <guga.santos.guga@gmail.com>
2022-07-30 14:11:56 -0300

    Feat: Pastas criadas + Footer e Header

---
+++ landingpage/src/context/LandingPageContext.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const LandingPageContext = createContext();
+
+export default LandingPageContext;
--- landingpage/src/index.js
+++ landingpage/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import LandingPageProvider from './context/LandingPageProvider';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LandingPageProvider>
+      <Router>
+        <App />
+      </Router>
+    </LandingPageProvider>
   </React.StrictMode>
 );
 
